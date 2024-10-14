4b1e16ca83c3f63687662657464dfef883500d31
mern4soft <mern4soft@gmail.com>
2024-10-14 16:33:17 +0530

    cors

--- Server.js
+++ Server.js
@@ -4,10 +4,12 @@ const dotenv = require('dotenv');
 const userRoutes = require('./Route/UserRoutes');
 const productRoutes = require('./Route/productRoutes');
 const orderRoutes = require('./Route/orderRoutes');
+const cors =require('cors')
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
